e8237177588e42e266cd8a96ee92dc43929fdb93
Mathias Novas <mathias_novas@hotmail.com>
2020-03-19 10:00:07 +0100

    Update image descriptions

--- pages/press.php
+++ pages/press.php
@@ -9,22 +9,22 @@
     <p>By the lovely <a href="https://www.instagram.com/toniagost/" target="_blank">toni</a>.</p>
 
     <figure data-aos="fade-up">
-        <img src="/assets/images/matty-3.JPG" alt="mattys doin smth">
+        <img src="/assets/images/matty-3.JPG" alt="Matty is sitting on a chair, holding a phone, and looking far into the future">
     </figure>
 
     <figure data-aos="fade-up">
-        <img src="/assets/images/matty-26.JPG" alt="mattys doin smth">
+        <img src="/assets/images/matty-26.JPG" alt="Matty is laying down, talking on the phone, and realising it's broken">
     </figure>
 
     <figure data-aos="fade-up">
-        <img src="/assets/images/matty-20 2.JPG" alt="mattys doin smth">
+        <img src="/assets/images/matty-20 2.JPG" alt="Matty is having a conversation with a plant">
     </figure>
 
     <figure data-aos="fade-up">
-        <img src="/assets/images/matty-12.JPG" alt="mattys doin smth">
+        <img src="/assets/images/matty-12.JPG" alt="Matty has gone a bit insane">
     </figure>
 
     <figure data-aos="fade-up">
-        <img src="/assets/images/matty-24.JPG" alt="mattys doin smth">
+        <img src="/assets/images/matty-24.JPG" alt="Flying phones are a thing now">
     </figure>
 </div>

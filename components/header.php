aba6d710e99807e8f6573fcce0b38408bc14a24f
Mathias Novas <mathias_novas@hotmail.com>
2020-09-18 13:30:32 +0200

    Add logo

--- components/header.php
+++ components/header.php
@@ -1,10 +1,11 @@
 <header class="header">
     <div class="container">
-        <h1 class="<?= $currentPage['slug'] === 'home' ? 'h-display' : '' ?>"><?= $currentPage['title']; ?></h1>
-
         <?php if ($currentPage['slug'] === 'home'): ?>
+            <figure class="front-page-logo"><img src="/assets/images/logo.png" alt="Earth Invader Logo"></figure>
             <h2 class="h-display">Hi Who's This</h2>
             <h3 class="h-display" style="margin-top: 0">Coming September 25th</h3>
+        <?php else: ?>
+            <h1><?= $currentPage['title']; ?></h1>
         <?php endif; ?>
     </div>
 </header>
